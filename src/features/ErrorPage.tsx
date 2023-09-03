fdb42697616f66d2e7fee97a13cfe9c4444c1754
inspel <abozhanov@gmail.com>
2023-09-03 14:29:53 +0200

    refactor: updates ErrorPage

--- src/features/ErrorPage.tsx
+++ src/features/ErrorPage.tsx
@@ -1,16 +1,19 @@
 import { useRouteError } from 'react-router-dom'
+import { Container, Heading, Text } from '@chakra-ui/react'
 
 export const ErrorPage = () => {
   const error = useRouteError() as Record<'statusText' | 'message', string>
   console.error(error)
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+    <Container id="error-page">
+      <Heading as="h1" size="2xl">
+        Oops!
+      </Heading>
+      <Text size="xl">Sorry, an unexpected error has occurred.</Text>
+      <Text size="xl">
         <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+      </Text>
+    </Container>
   )
 }
